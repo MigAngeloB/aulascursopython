33dab4ace73418ab2816cdfc4b43264e18bb2a95
Miguel Angelo Bertolo Campos <00001110756926sp@al.educacao.sp.gov.br>
2025-04-29 12:12:00 -0300

    Cadastro nome e idade

--- function.py
+++ function.py
@@ -13,3 +13,19 @@ def multiplicarDoisNumeros(nota01, nota02):
     return print (resultado)
 
 multiplicarDoisNumeros(10,10)
+
+nota03 = 200
+nota04 =145
+
+def multiplicarDoisNUmeros (nota03, nota04):
+    resultado = nota03 * nota04
+
+    return print(resultado)
+
+def cadastrar (nome, idade):
+    data = {
+        "nome": nome,
+        "idade": idade,
+    }
+    return print(data)
+cadastrar("Miguel", "16")
