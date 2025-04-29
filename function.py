04e305a5a571398cf75b902e3fb7d107cff9deba
Miguel Angelo Bertolo Campos <00001110756926sp@al.educacao.sp.gov.br>
2025-04-29 10:16:13 -0300

    sistema de multiplicação e soma de numeros

---
+++ function.py
@@ -0,0 +1,15 @@
+def soma():
+    nun01 = 10
+    nun02 = 20
+    resultado = nun01 + nun02
+
+    return resultado
+
+def multiplicarDoisNumeros(nota01, nota02):
+    
+
+    resultado = nota01 * nota02
+
+    return print (resultado)
+
+multiplicarDoisNumeros(10,10)
