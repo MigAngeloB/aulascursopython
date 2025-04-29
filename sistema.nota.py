ebd402e0dab987bf71b815462989a43839da15b4
Miguel Angelo Bertolo Campos <00001110756926sp@al.educacao.sp.gov.br>
2025-04-29 09:48:35 -0300

    sistema de soma da nota

---
+++ sistema.nota.py
@@ -0,0 +1,9 @@
+print ("Sistema de soma: ")
+nota1 = int (input("Digite a nota 1: "))
+nota2 = int (input("Digite a nota 2: "))
+nota3 = int (input("Digite a nota 3: "))
+nota4 = int (input("Digite a nota 4: "))
+
+resultado = nota1+nota2+nota3+nota4
+
+print (resultado)
